09ee72566af06e3a751ff511e63eb1ad7f61fc93
Tanguy BENOIST <tbenoist@e3r4p15.42.fr>
2015-12-21 11:04:37 +0100

    check

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: tbenoist <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/11/24 14:54:00 by tbenoist          #+#    #+#              #
-#    Updated: 2015/12/21 09:48:09 by tbenoist         ###   ########.fr        #
+#    Updated: 2015/12/21 10:23:48 by tbenoist         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,8 +18,8 @@ OBJ = $(SRC:.c=.o)
 
 all: $(NAME)
 
-$(NAME) : $(SRC) 
-	@gcc -Wall -Wextra -Werror $(SRC)
+$(NAME) : $(SRC)
+	@gcc -o $(NAME) -Wall -Wextra -Werror $(SRC)
 
 clean:
 
--- fillit.c
+++ fillit.c
@@ -6,20 +6,52 @@
 /*   By: tbenoist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/21 09:52:51 by tbenoist          #+#    #+#             */
-/*   Updated: 2015/12/21 10:20:44 by tbenoist         ###   ########.fr       */
+/*   Updated: 2015/12/21 11:03:56 by tbenoist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #define BUFF_SIZE 21
 
+int		check(char *buf)
+{
+	int		i;
+	int		row;
+	int		col;
+
+	i = 0;
+	row = 0;
+	col = 0;
+	while ((buf[i] == '.' || buf[i] == '\n' || buf[i] == '#') && i < 20)
+	{
+		if (buf[i] == '.' || bif[i] == '#');
+			col++;
+		if (buf[i] == '\n' && buf[i - 1] != '\n')
+		{
+			row++;
+			if (col != 4)
+				return (0);
+			col = 0;
+		}
+		i++;
+	}
+	if (row != 4 || i != 20)
+		return (0);
+	return (1);
+}
+
 int		main(int argc, char **argv)
 {
 	int		fd;
+	char	buf[BUFF_SIZE];
 
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_CREAT, O_RDONLY);
+		while(read(fd, buf, BUFF_SIZE))
+		{
+			check(buf);
+		}
 	}
 	return (0);
 }
